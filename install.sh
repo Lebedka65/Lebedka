2f8f46cca88ccc8a57a511e58173cfd22355231a
Lebedka65 <89278607+Lebedka65@users.noreply.github.com>
2021-08-23 00:06:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,13 +11,13 @@ then
         pkg install python
         pkg install dos2unix
         pip install requests colorama proxyscrape
-        cp ~/lebedka/spamer.py $PREFIX/bin/spamer
+        cp ~/Lebedka/spamer.py $PREFIX/bin/spamer
         dos2unix $PREFIX/bin/spamer
         chmod -R 777 ~/spamer
         chmod 777 $PREFIX/bin/spamer
         spamer
         else
-                        echo "Некорректный ввод"
-                     fi
-                fi
-          fi
+                        
+                     
+                
+          
