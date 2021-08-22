167f210bb4bc9eeb4793c744e026d4aca0f05f45
Lebedka65 <89278607+Lebedka65@users.noreply.github.com>
2021-08-22 23:36:53 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+echo "--------------------"
+echo "|     Кто ты ?     |"
+echo "|------------------|"
+echo "| 1. Termux        |"
+echo "--------------------"
+read numb
+if [ $numb = "1" ]
+then
+pkg install python
+pkg install dos2unix
+pip install requests colorama proxyscrape
+cp ~/spymer/spammer.py $PREFIX/bin/spymer
+dos2unix $PREFIX/bin/spymer
+chmod -R 777 ~/spymer
+chmod 777 $PREFIX/bin/spymer
+spymer
