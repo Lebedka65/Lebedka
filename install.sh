ea055cdbabcd664df741129c23596b41f1f2d3fc
Lebedka65 <89278607+Lebedka65@users.noreply.github.com>
2021-08-23 00:12:40 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,7 @@ echo "|     Кто ты ?     |"
 echo "|------------------|"
 echo "| 1. Termux        |"
 echo "--------------------"
-read numb
-if [ $numb = "1" ]
-then
+
         pkg install python
         pkg install dos2unix
         pip install requests colorama proxyscrape
