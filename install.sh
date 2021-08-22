881af9508a5d07bef1b92895770a665128bb60f0
Lebedka65 <89278607+Lebedka65@users.noreply.github.com>
2021-08-23 00:00:05 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,16 +8,16 @@ echo "--------------------"
 read numb
 if [ $numb = "1" ]
 then
-pkg install python
-pkg install dos2unix
-pip install requests colorama proxyscrape
-cp ~/lebedka/spamer.py $PREFIX/bin/spamer
-dos2unix $PREFIX/bin/spamer
-chmod -R 777 ~/spamer
-chmod 777 $PREFIX/bin/spamer
-spamer
-else
-echo "Некорректный ввод"
-fi
-fi
-fi
+        pkg install python
+        pkg install dos2unix
+        pip install requests colorama proxyscrape
+        cp ~/lebedka/spamer.py $PREFIX/bin/spamer
+        dos2unix $PREFIX/bin/spamer
+        chmod -R 777 ~/spamer
+        chmod 777 $PREFIX/bin/spamer
+        spamer
+        else
+                        echo "Некорректный ввод"
+                     fi
+                fi
+          fi
